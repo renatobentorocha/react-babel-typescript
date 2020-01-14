77b2d401bf73f3ccd90f3f9ad66e137c901d2457
Renato Rocha <renatob.rocha@gmail.com>
2020-01-14 17:06:08 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/avatar.png';
+import '../styles.css';
+
+export default function src() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <img src={logo} alt="" />
+    </div>
+  );
+}
